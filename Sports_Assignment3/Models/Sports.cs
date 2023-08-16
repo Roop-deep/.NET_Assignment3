09385aeda64221fc463f8beb6a2d99d4675e68ea
Gurbir kaur <gurbirkaur1002@gmail.com>
2023-08-16 10:12:33 -0400

    First commit

---
+++ Sports_Assignment3/Models/Sports.cs
@@ -0,0 +1,15 @@
+﻿namespace Sports_Assignment3.Models
+{
+    public class Sports
+    {
+        public string? ID { get; set; }
+
+        public string? SportName { get; set; }
+
+        public string? SportType { get; set; }
+
+        public int NumberofPlayers { get; set; }
+
+        public string? CountryOfOrigin { get; set; }
+    }
+}
